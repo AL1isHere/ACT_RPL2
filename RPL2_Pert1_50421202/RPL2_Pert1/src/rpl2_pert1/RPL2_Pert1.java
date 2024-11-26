26ad8e9f5f6d0319f6be2ae6df8b1d3c379f4a3c
AL1isHere <arifin,bolt27@gmail.com>
2024-11-26 11:55:24 +0700

    first commit

---
+++ RPL2_Pert1_50421202/RPL2_Pert1/src/rpl2_pert1/RPL2_Pert1.java
@@ -0,0 +1,13 @@
+package rpl2_pert1;
+
+public class RPL2_Pert1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Printable component = new Component();
+        component.print();
+    }
+    
+}
---
+++ RPL2_Pert1_50421202/src/rpl2_pert1/Component.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rpl2_pert1;
+
+/**
+ *
+ * @author Arifin
+ */
+public class Component implements Printable{
+    @Override 
+    public void print(){
+        System.out.println("Nama    : Arifin Ilham Muttaqin");
+        System.out.println("NPM     : 50421202");
+        System.out.println("Kelas   : 4IA06");
+    }
+}
